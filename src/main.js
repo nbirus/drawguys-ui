024a0bfd57489b9bbb9a2d079e6d2c56b381f6e3
Nathan Birus <nbirus@harding.edu>
2020-09-28 20:13:28 -0500

    set favicon

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import router from "./router.js";
 import "./styles/index.scss";
 import 'remixicon/fonts/remixicon.css'
 
+let favicon = document.querySelector('#fav')
+if (favicon && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  favicon.href = '/logo-dark.png'
+}
+
 createApp(App)
   .use(router)
   .mount("#app");
